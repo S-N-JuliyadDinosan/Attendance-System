45e9aa33f6390fd9acd5bc12c3cbd568cd04a4ae
S N Juliyad Dinosan <brackdino90@gmail.com>
2025-05-29 03:11:26 +0530

    #2.9, Created Endpointbundle.java for managing End points

---
+++ server/src/main/java/com/_axislabs/attendance_system/util/EndpointBundle.java
@@ -0,0 +1,10 @@
+package com._axislabs.attendance_system.util;
+
+public class EndpointBundle {
+    public static final String BASE_URL = "/api/v1";
+
+    //UserController
+    public static final String USER = BASE_URL + "/users";
+    public static final String LOGIN_USER = "/login";
+
+}
