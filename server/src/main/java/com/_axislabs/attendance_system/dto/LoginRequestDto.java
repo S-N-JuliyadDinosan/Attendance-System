b2a974695e5a679f6b055c10927b53baa8a9fe98
S N Juliyad Dinosan <brackdino90@gmail.com>
2025-05-29 01:45:39 +0530

    #2.3, Created LoginRequestDto and added validation dependencies to pom.xml

---
+++ server/src/main/java/com/_axislabs/attendance_system/dto/LoginRequestDto.java
@@ -0,0 +1,16 @@
+package com._axislabs.attendance_system.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequestDto {
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
--- server/src/main/java/com/_axislabs/attendance_system/dto/UserDTO.java
+++
@@ -1,9 +0,0 @@
-package com._axislabs.attendance_system.dto;
-
-import lombok.Data;
-
-@Data
-public class UserDTO {
-    private String username;
-    private String password;
-}
